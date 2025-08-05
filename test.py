34c4b5ad1f2e4baad0e6c32938878ea66abdb15c
pedro Hernandez <phernandez804@soyudemedellin.edu.co>
2025-08-05 12:21:30 -0500

    extraordinary

--- test.py
+++ test.py
@@ -27,5 +27,38 @@ class pruebas_credito(unittest.TestCase):
         _, _, _, total = conversion_tasa_anual(monto_credito, duracion_periodo_meses, tasa_interes_anual, plazo_amortizacion)    
         self.assertEqual(round(total, 2), 39967327.37)
 
+    def test_normal_3(self):
+        #entradas
+        monto_credito = 25000000
+        duracion_periodo_meses = 72
+        tasa_interes_anual = 12
+        plazo_amortizacion = 140
+
+        #probar proceso de conversion
+        _, _, _, total = conversion_tasa_anual(monto_credito, duracion_periodo_meses, tasa_interes_anual, plazo_amortizacion)
+        self.assertEqual(round(total, 2), 51177482.8)
+
+    def test_extraordinary_1(self):
+        #entradas
+        monto_credito = 15000000
+        duracion_periodo_meses = 36
+        tasa_interes_anual = 10
+        plazo_amortizacion = 180
+
+        #probar proceso de conversion
+        _, _, _, total = conversion_tasa_anual(monto_credito, duracion_periodo_meses, tasa_interes_anual, plazo_amortizacion)
+        self.assertEqual(round(total, 2), 20222727.64)
+
+    def test_extraordinary_2(self):
+        #entradas
+        monto_credito = 25000000
+        duracion_periodo_meses = 60
+        tasa_interes_anual = 15
+        plazo_amortizacion = 120
+
+        #probar proceso de conversion
+        _, _, _, total = conversion_tasa_anual(monto_credito, duracion_periodo_meses, tasa_interes_anual, plazo_amortizacion)
+        self.assertEqual(round(total, 2), 52679533.67)
+
 if __name__ == '__main__':
     unittest.main()
